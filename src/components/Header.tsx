db3410429cef3b45a0f6853603732791d271fe58
BriXelDev <briangar23@outlook.com>
2026-01-01 14:00:21 -0600

    Primer estructura de la página

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import './styles/Header.css'
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>Github Random Header</h1>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import './styles/Hero.css'
+
+function Hero() {
+    return (
+        <section className="hero">
+            <h1>Github Random</h1>
+        </section>
+    );
+}
+
+export default Hero;
