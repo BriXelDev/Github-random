203614f9f9001409ae3e7ef1b8d79702ac8da68b
BriXelDev <briangar23@outlook.com>
2026-01-01 14:31:55 -0600

    Base del formulario

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,41 @@
 import './styles/Hero.css'
+import { useState } from 'react';
+import { searchRepositories, getRandomRepository } from '../services/githubService';
+import type { Repository } from '../services/githubService';
 
 function Hero() {
+    const [language, setLanguage] = useState('javascript');
+    const [repo, setRepo] = useState<Repository | null>(null);
+    const [loading, setLoading] = useState(false);
+
+    const handleSearch = async () => {
+        setLoading(true);
+        try{
+            const repos = await searchRepositories(language);
+            const random = getRandomRepository(repos);
+            setRepo(random);
+        } catch (error){
+            console.error('Error: ', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
-        <section className="hero">
+        <section className="hero"> 
             <h1>Github Random</h1>
+            <div className="search-container"> 
+                <input 
+                    type="text"
+                    placeholder="Lenguaje de programación (ej: javascript, python, rust)"
+                    value={language}
+                    onChange={(e) => setLanguage(e.target.value)}
+                />
+                <button onClick={handleSearch} disabled={loading}>{loading ? 'Buscando...' : 'Buscar Repositorio Aleatorio'}</button>
+            </div>
+        
         </section>
+
     );
 }
 
